590b9158d3bc750a3019411c650f19de4811c8c6
Katherine Mcneil <w0417834@campus.nscc.ca>
2025-01-23 15:19:32 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+let height=170;
+const userAge=30;
+console.log(" answer on part 1");
+console.log(" height variable is " , height );
+console.log(" user Age variable is " , userAge );
+console.log("answer on part 2");
+
+const petName = [
+"geronimo",
+"Charlotte",
+"Sophie"
+ 
+ 
+];
+ 
+console.log(" array after creating " , petName );
+petName.push("Logan");
+console.log(" array after using push " , petName );
+petName.pop();
+console.log(" array after using pop " , petName );
+petName.reverse();
+console.log(" array after using reverse " , petName );
+
+let car = {
+make: "Toyota"  ,
+model: "RAV4"  ,
+year: 2025
+ 
+ 
+};
+ 
+console.log(" car info is as follow " , car );
+car.year =2024;
+console.log(" car info after updating the year " , car );
+ 
+car.color = "red";
+console.log(" car info after adding color as new attribute " , car );
